8341b34ee8128b7c1bb85c95805518074833fd8c
Prajwal <prajwalkaradbhuje@gmail.com>
2021-05-05 10:47:47 +0530

    first commit normal design

---
+++ DigiClock.js
@@ -0,0 +1,30 @@
+showtime();
+
+function showtime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session ="AM";
+
+    if (h==0) {
+        h=12;
+        session="AM";
+    }
+
+    if (h>12) {
+        h=h-12;
+        session="PM";
+    }
+
+    h=(h<10)?"0" +h:h;
+    m=(m<10)?"0" +m:m;
+    s=(s<10)?"0" +s:s;
+
+    var time = h+":"+m+":"+s+" "+session;
+    document.getElementById("ClockDisplay").innerText =time;
+    document.getElementById("ClockDisplay").textContent=time;
+
+    setTimeout(showtime,1000);
+}
+
